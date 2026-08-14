e59709b3b7a626d4b8eb7b7d51d55b5ddb89e665
Pythoooo0 <ju.muetzel@gmx.de>
2026-08-14 14:29:02 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE='klassencloud-v16';
+const APP_SHELL=['/','/index.html','/css/style.css','/js/app.js','/manifest.json','/icon-192.png','/icon-512.png'];
+self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(c=>c.addAll(APP_SHELL)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',event=>{
+  const req=event.request;
+  if(req.method!=='GET') return;
+  const url=new URL(req.url);
+  if(url.origin!==location.origin) return;
+  if(url.pathname.startsWith('/api/')) return;
+  event.respondWith(fetch(req).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put(req,copy));return res}).catch(()=>caches.match(req).then(r=>r||caches.match('/index.html'))));
+});
+self.addEventListener('notificationclick',event=>{
+ event.notification.close();
+ event.waitUntil(clients.matchAll({type:'window',includeUncontrolled:true}).then(cs=>{
+   const c=cs.find(x=>x.url.startsWith(self.location.origin));
+   return c?c.focus():clients.openWindow('/');
+ }));
+});
